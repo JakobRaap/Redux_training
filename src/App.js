2941eb04bdbe84ea86078bbe0ec87c223723a5cc
Jakob Raap <bokaj.paar@gmail.com>
2023-07-31 18:46:08 +0200

    pagination added

--- src/App.js
+++ src/App.js
@@ -5,21 +5,35 @@ import RestaurantCard from "./components/RestaurantCard";
 import { overlayState } from "./features/overlayBackground/overlayBackgroundSlice";
 import { useSelector } from "react-redux";
 import OverlayBackground from "./features/overlayBackground/OverlayBackground";
+import { Pagination } from "./components/Pagination";
+import { useState } from "react";
 
 function App() {
-  const { data, isLoading } = useGetBreweriesWithLimitQuery(50);
-
+  const { data, isLoading } = useGetBreweriesWithLimitQuery(55);
   const isOverlay = useSelector(overlayState);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+  function paginate(page = 1) {
+    setCurrentPage(page);
+  }
+  const indexOfLastCard = currentPage * itemsPerPage;
+  const indexOfFirstCard = indexOfLastCard - itemsPerPage;
 
+  if (isLoading) return "Loading.....";
+  const restaurantsToShow = data.slice(indexOfFirstCard, indexOfLastCard);
+  console.log(restaurantsToShow);
   return (
     <>
       {isOverlay && <OverlayBackground />}
-
-      {isLoading
-        ? "loading"
-        : data.map((restaurant) => (
-            <RestaurantCard key={restaurant.id} r={restaurant} />
-          ))}
+      <Pagination
+        itemsPerPage={itemsPerPage}
+        totalItems={data.length}
+        paginate={paginate}
+      ></Pagination>
+      <h2>current page: {currentPage}</h2>
+      {restaurantsToShow.map((restaurant) => (
+        <RestaurantCard key={restaurant.id} r={restaurant} />
+      ))}
     </>
   );
 }
---
+++ src/components/Pagination.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const Pagination = ({ totalItems, itemsPerPage, paginate }) => {
+  const pageNumbers = [];
+  const calculatedPageNumbers = Math.ceil(totalItems / itemsPerPage);
+
+  for (let i = 1; i <= calculatedPageNumbers; i++) {
+    pageNumbers.push(i);
+  }
+  console.log(totalItems, itemsPerPage, calculatedPageNumbers, pageNumbers);
+  return (
+    <nav>
+      {pageNumbers.map((number) => (
+        <a
+          href="!#"
+          style={{ margin: "4px", fontSize: "30px" }}
+          key={number}
+          onClick={() => paginate(number)}
+        >
+          {number}
+        </a>
+      ))}
+    </nav>
+  );
+};
